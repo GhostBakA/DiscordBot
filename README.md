65288ff6bfddb80bb1ab3539854bfd7343628e55
GhostBaka <manan1206@gmail.com>
2020-04-07 16:06:05 +0530

    Added a cron job for heroku

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # DiscordBot
-1. Create a file auth.json in your root directory with content
-{
-"token":"Your Discord Bot Token"
-}
+1. Create .env file in your root directory with content
+
+AUTH_TOKEN:Your Discord Bot Token
 
 2. npm i
+
 3. node .
--- bot.js
+++ bot.js
@@ -5,11 +5,37 @@ const app = express()
 const port = process.env.PORT || 3000;
 dotenv.config();
 
+//Cron Job to keep Heroku App Alive
+var cron = require('node-cron');
+var https = require('https');
+
+cron.schedule('0,30 * * * *', () => {
+    const options = {
+        hostname: 'bakchod-bot.herokuapp.com',
+        path: '/',
+        method: 'GET'
+    }
+
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+            console.log('Success')
+        })
+    })
+
+    req.on('error', error => {
+        console.error(error)
+    })
+
+    req.end()
+});
+
 // Initialize Discord Bot
 authToken = process.env.AUTH_TOKEN
 var bot = new Discord.Client();
 bot.on('ready', function (evt) {
-    console.log("Bot status : "+bot.presence.status)
+    console.log("Bot status : " + bot.presence.status)
 });
 bot.on('message', message => {
     if (message.author.bot == false) {
@@ -17,13 +43,13 @@ bot.on('message', message => {
             reply = message.content.replace("!r", "")
             message.channel.send(reply)
         }
-        else if(message.content.slice(0, 2) == '!s'){
+        else if (message.content.slice(0, 2) == '!s') {
             reply = message.content.replace("!s", "")
-            for(i=0; i<20; i++){
+            for (i = 0; i < 20; i++) {
                 message.channel.send(reply)
             }
         }
-        else if(message.content.slice(0,7) == 'bakchod'){
+        else if (message.content.slice(0, 7) == 'bakchod') {
             console.log(message.member.voice.channel)
             message.member.voice.channel.join()
         }
@@ -32,9 +58,8 @@ bot.on('message', message => {
 
 bot.login(authToken);
 
-
 app.get('/', (req, res) => {
-    res.send("Bot Status : "+bot.presence.status)
+    res.send("Bot Status : " + bot.presence.status)
 })
 
-app.listen(port, () => console.log("App listening at 'localhost:" + port+"'"))
+app.listen(port, () => console.log("App listening at 'localhost:" + port + "'"))
